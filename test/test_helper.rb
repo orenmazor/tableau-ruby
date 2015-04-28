a4d5a9309cf198132f57dd13dbfea3dc47f149fe
Oren Mazor <oren.mazor@gmail.com>
2015-04-28 16:05:03 +0000

    use tableautest to encapsulate meta shit

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,11 @@ require "minitest/autorun"
 require 'byebug'
 require 'tableau_ruby'
 require 'mocha/setup'
+
+class TableauTest < Minitest::Test
+  def setup
+    @client = Tableau::Client.new(host: ENV['TABLEAU_URL'], admin_name: ENV['TABLEAU_ADMIN_USER'], admin_password: ENV['TABLEAU_ADMIN_PASSWORD'])
+    debugger
+    @admin_user = @client.users.find_by(ENV['TABLEAU_ADMIN_USER'])
+  end
+end
--- test/test_site.rb
+++ test/test_site.rb
@@ -1,10 +1,6 @@
 require File.dirname(__FILE__) + '/test_helper'
 
-class TestSite < Minitest::Test
-  def setup
-    @client = Tableau::Client.new(host: ENV['TABLEAU_URL'], admin_name: ENV['TABLEAU_ADMIN_USER'], admin_password: ENV['TABLEAU_ADMIN_PASSWORD'])
-  end
-
+class TestSite < TableauTest
   def test_site_listing
     sites = @client.sites.all 
     assert sites[:sites].is_a? Array
