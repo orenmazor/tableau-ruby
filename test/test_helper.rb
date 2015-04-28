dda7889e6ce50fe356c8cbd8464ca77a8d52102b
Oren Mazor <oren.mazor@gmail.com>
2015-04-28 17:24:22 +0000

    normalize site parsing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require 'mocha/setup'
 class TableauTest < Minitest::Test
   def setup
     @client = Tableau::Client.new(host: ENV['TABLEAU_URL'], admin_name: ENV['TABLEAU_ADMIN_USER'], admin_password: ENV['TABLEAU_ADMIN_PASSWORD'])
-    debugger
-    @admin_user = @client.users.find_by(ENV['TABLEAU_ADMIN_USER'])
+    @site = @client.sites.find_by(name: ENV['TABLEAU_DEFAULT_SITE'])
+    # @admin_user = @client.users.find_by(ENV['TABLEAU_ADMIN_USER'])
   end
 end
--- test/test_site.rb
+++ test/test_site.rb
@@ -1,6 +1,12 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class TestSite < TableauTest
+  def test_site_finding
+    site = @client.sites.find_by(name: ENV['TABLEAU_DEFAULT_SITE'])
+    assert site[:name]
+    assert site[:id]
+  end
+
   def test_site_listing
     sites = @client.sites.all 
     assert sites[:sites].is_a? Array
